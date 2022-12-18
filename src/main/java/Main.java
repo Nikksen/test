f431cd4b7dfc7a9a1d16987f84866d3c537ba015
nikito <aa@aa.aa>
2022-12-18 12:48:45 +0200

    regbi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int counterShuttles = 0;
-        for (int i = 0; ; i++) {
-            if (i / 10 == 4 || i / 10 == 9 ||
-                    i / 10 == 14 || i / 10 == 19 || i % 10 == 4 || i % 10 == 9) {
-                continue;
-            } else {
-                System.out.println("Вы ввели не целочисленое значение, попробуйте снова");
-                counterShuttles++;
-            }
-            if (counterShuttles == 100) {
-                break;
-            }
-            System.out.println(i);
+        int sumAverage1 = 0;
+        int sumAverage2 = 0;
+        int min = 18;
+        int max = 40;
+        int[] firstTeam = new int[25];
+        int[] ageTeam2= new int[25];
+
+        for (int i=0; i < firstTeam.length;i++){
+            firstTeam[i] = (int) (Math.random()*(40 - 18) + 18);
+            ageTeam2[i] = (int) (Math.random()*(40 - 18) + 18);
+
+            System.out.print(firstTeam[i]);
+            System.out.println();
+            sumAverage1 += firstTeam[i]; // тоже самое что sumAverage1 = sumAverage1 + firstTeam[i];
+            sumAverage2 += ageTeam2[i];
         }
 
+        double average1 = (double) sumAverage1 / firstTeam.length;
+        double average2 = (double) sumAverage2 / firstTeam.length;
     }
 }
